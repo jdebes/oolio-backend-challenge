d1146997e164750abcdb4727076be86c3560ac3f
Jonas Debes <jpdebes@gmail.com>
2025-02-04 14:10:08 +1100

    Add commands to run valid promo or server

--- main.go
+++ main.go
@@ -3,27 +3,50 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"jdebes/oolio-backend-challenge/data"
 	"jdebes/oolio-backend-challenge/handlers"
 	"jdebes/oolio-backend-challenge/middleware"
+)
+
+const (
+	dataArg   = "data"
+	serverArg = "server"
 )
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.DebugLevel)
 
-	base := handlers.NewBaseHandler()
+	if len(os.Args) < 2 {
+		log.Panicf("Requires argument '%s' or '%s'", dataArg, serverArg)
+		os.Exit(1)
+	}
 
-	r := mux.NewRouter()
+	switch cmd := os.Args[1]; cmd {
+	case dataArg:
+		err := data.WriteValidPromos()
+		if err != nil {
+			log.Panic("Failed write valid promos", err)
+		}
+	case serverArg:
+		base := handlers.NewBaseHandler()
 
-	r.Use(middleware.AuthMiddleware)
+		r := mux.NewRouter()
 
-	r.HandleFunc("/product/{productId}", base.GetProduct).Methods(http.MethodGet)
-	r.HandleFunc("/product", base.ListProducts).Methods(http.MethodGet)
-	r.HandleFunc("/order", base.PlaceOrder).Methods(http.MethodPost)
+		r.Use(middleware.AuthMiddleware)
 
-	fmt.Println("Server starting on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+		r.HandleFunc("/product/{productId}", base.GetProduct).Methods(http.MethodGet)
+		r.HandleFunc("/product", base.ListProducts).Methods(http.MethodGet)
+		r.HandleFunc("/order", base.PlaceOrder).Methods(http.MethodPost)
+
+		fmt.Println("Server starting on port 8080...")
+		log.Fatal(http.ListenAndServe(":8080", r))
+	default:
+		log.Errorf("Invalid argument, provide '%s' or '%s'", dataArg, serverArg)
+		os.Exit(1)
+	}
 }
