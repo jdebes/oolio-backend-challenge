75966c462cd64e621c8e8a33af89e7b2d848f18b
Jonas Debes <jpdebes@gmail.com>
2025-02-04 14:12:27 +1100

    tidy up auth.go

--- middleware/auth.go
+++ middleware/auth.go
@@ -5,16 +5,22 @@ import (
 	"strings"
 )
 
+const (
+	// Should be placed in secrets store
+	validApiSecret = "apitest"
+	apiKeyHeader   = "api_key"
+)
+
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		apiKey := r.Header.Get("api_key")
+		apiKey := r.Header.Get(apiKeyHeader)
 
 		if apiKey == "" {
 			http.Error(w, "Missing API key", http.StatusUnauthorized)
 			return
 		}
 
-		if strings.TrimSpace(apiKey) != "apitest" {
+		if strings.TrimSpace(apiKey) != validApiSecret {
 			http.Error(w, "Invalid API key", http.StatusUnauthorized)
 			return
 		}
